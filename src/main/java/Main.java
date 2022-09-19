2617bc68243bbed3ed0ee607a9ae600dc43d2a1b
modengann <99417989+modengann@users.noreply.github.com>
2022-09-18 22:03:59 -0400

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+/**
+ *  Youtube:  https://www.youtube.com/watch?v=jRV42TjmRhM&list=PLpkMWkvwtatjEEHLbKGp1HJOggcz2O4sM&index=9
+ *  Goals:
+ *  1. Copy your entire code from Methods 1 and place it below this comment
+ *  2. Adjust your smileyFace method so that it will take a Location parameter
+ *  3. Update your code so that the smileyFace method will draw the smileyFace at the Location provided
+ *     We should be able to then call smileyFace from a pre-defined method like mouseDown()
+ */
+
+import javadraw.*;
+public class Main extends Window{
+    
+     public static void main(String[] args) {
+       
+        Location loc = new Location(600,100);
+        printSmiley(loc);
+    }
+
+    public static void printSmiley(Location location){
+
+    }
+    
+}
